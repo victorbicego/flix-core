69aec387390a6b888c386ec68eea3a3fe70af123
bice023 <victor.bicegoquintaneira@riverty.com>
2024-04-05 20:24:45 +0200

    add christian module

---
+++ christian-flix/src/main/java/com/myflix/christianflix/ChristianFlixApplication.java
@@ -0,0 +1,13 @@
+package com.myflix.christianflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristianFlixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChristianFlixApplication.class, args);
+    }
+
+}
---
+++ christian-flix/src/test/java/com/myflix/christianflix/ChristianFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myflix.christianflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristianFlixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
